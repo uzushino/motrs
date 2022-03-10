d54a6312bd5bbd10bd65e28e72a2bfdf039336ef
uzushino <torattoria.mamiya@gmail.com>
2022-03-10 22:36:57 +0900

    create instancel tracker

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,7 +1,24 @@
 use filter::kalman::kalman_filter::KalmanFilter;
-use nalgebra::{U1, U2, Vector2, Matrix2, Matrix1};
+use nalgebra::base::Vector1;
+use nalgebra::{dmatrix, DVector, DMatrix, Matrix2x1, dvector, VectorN, MatrixMN, RealField, Dim};
+use nalgebra::base::dimension::DimName;
 
-fn get_kalman_object_tracker() -> KalmanFilter<f64, U1, U1, U1> {
-    let mut tracker = KalmanFilter::<f64, U1, U1, U1>::default();
+use crate::model::Model;
+
+fn get_kalman_object_tracker<F, DimX, DimZ, DimU>(model: &Model, x0: Option<DVector<f64>>) -> KalmanFilter<f32, U1, U1, U1>
+    where
+        F: RealField,
+        DimX: DimName,
+        DimZ: DimName,
+        DimU: DimName,
+{
+    let mut tracker = KalmanFilter::<f64, DimX, DimZ, DimU>::default();
+
+    tracker.F = model.build_F();
+    tracker.Q = model.build_Q();
+    tracker.H = model.build_H();
+    tracker.R = model.build_R();
+    tracker.P = model.build_P();
+
     tracker
 }

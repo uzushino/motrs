7092708420da9d56449a5852b8eb10bd77a2e7c3
uzushino <torattoria.mamiya@gmail.com>
2022-03-14 13:27:54 +0900

    add filter fork repo

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,15 +1,9 @@
 use filter::kalman::kalman_filter::KalmanFilter;
-use nalgebra::allocator::Allocator;
-use nalgebra::base::dimension::DimName;
 use nalgebra::{DefaultAllocator, DVector, MatrixMN, RealField, VectorN};
 
 use crate::model::Model;
 
 fn get_kalman_object_tracker<F, DimX, DimZ, DimU>(model: &Model, x0: Option<DVector<f64>>) -> KalmanFilter<f64, DimX, DimZ, DimU>
-    where
-        DimX: DimName,
-        DimZ: DimName,
-        DimU: DimName,
 {
     let mut tracker = KalmanFilter::<f64, DimX, DimZ, DimU>::default();
 

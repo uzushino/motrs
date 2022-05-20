379383191ab46185f352da2b98434d07e36890fb
uzushino <torattoria.mamiya@gmail.com>
2022-05-20 23:58:04 +0900

    add examples package

--- examples/mot/src/main.rs
+++ examples/mot/src/main.rs
@@ -1,8 +1,14 @@
+use motrs::tracker::Track;
+
 use iced::{
     button, futures, image, Alignment, Application, Button, Column, Command,
     Container, Element, Length, Row, Settings, Text,
 };
 
-fn main() {
+fn draw_rectangle() -> i64 {
+    image::Handle::from_pixels(width, height, pixels)
+}
 
+
+fn main() {
 }

9013f00c3c1069eeaa6e3a0958fd875c7125eff1
uzushino <torattoria.mamiya@gmail.com>
2022-08-29 21:22:49 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "./motrs_wasm/"),
+      outDir: path.resolve(__dirname, "./motrs_wasm/pkg/"),
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true
+  },
+  mode: "development",
+};

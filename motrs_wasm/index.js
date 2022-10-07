1e74800ed1bd367c659033b759013666354744a4
uzushino <torattoria.mamiya@gmail.com>
2022-10-07 13:04:25 +0900

    js binding

--- motrs_wasm/index.js
+++ motrs_wasm/index.js
@@ -1,5 +1,21 @@
 const rust = import("./pkg");
 
 rust.then((m) => {
-    let ann = m.Mot.new();
+    let mot = m.Mot.new();
+
+    for (let i = 0; i < 10; i ++) {
+        let box = [1, 1, 1, 1];
+        let det = {
+            box,
+            score: 1,
+            class_id: 1,
+            feature: 1,
+        }
+
+        mot.step([det])
+
+        let tracks = mot.active_tracks()
+
+        console.log(tracks)
+    }
 })

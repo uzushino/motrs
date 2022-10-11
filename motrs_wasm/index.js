8b20c9018096bac1650eab5b949fe03ac4bd2467
uzushino <torattoria.mamiya@gmail.com>
2022-10-11 19:05:02 +0900

    add module

--- motrs_wasm/index.js
+++ motrs_wasm/index.js
@@ -1,12 +1,13 @@
 const rust = import("./pkg");
 
 rust.then((m) => {
-    let mot = m.Mot.new();
+    let mot = m.MOT.new();
 
     for (let i = 0; i < 10; i ++) {
         let box = [1, 1, 1, 1];
+
         let det = {
-            box,
+            _box: box,
             score: 1,
             class_id: 1,
             feature: 1,
@@ -14,8 +15,7 @@ rust.then((m) => {
 
         mot.step([det])
 
-        let tracks = mot.active_tracks()
-
-        console.log(tracks)
+        //let tracks = mot.active_tracks()
+        //console.log(tracks)
     }
 })
--- motrs_wasm/src/lib.rs
+++ motrs_wasm/src/lib.rs
@@ -1,10 +1,19 @@
 use motrs::model::*;
 use motrs::tracker::*;
 use wasm_bindgen::prelude::*;
+use serde_derive::{Serialize, Deserialize};
+use gloo_utils::format::JsValueSerdeExt;
+use web_sys::console;
+use nalgebra as na;
 
 #[wasm_bindgen]
 pub struct MOT {
     tracker: MultiObjectTracker,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Box {
+    pub _box: Vec<f32>,
 }
 
 #[wasm_bindgen]
@@ -35,5 +44,31 @@ impl MOT {
         );
 
         Self { tracker }
+    }
+
+    pub fn step(&mut self, val: &JsValue) {
+        let _box: Vec<Box> = val
+            .into_serde()
+            .unwrap();
+
+        console::log_1(&format!("len: {}", _box.len()).into());
+
+        let _box = na::DMatrix::from_row_slice(1, 4, &_box[0]._box);
+
+        let dets = Detection {
+            _box: Some(_box.clone()),
+            score: 1.,
+            class_id: 1,
+            feature: None,
+        };
+
+        self.tracker.step(vec![dets]);
+    }
+
+    pub fn active_tracks(&mut self) {
+        let tracks = self.tracker.active_tracks();
+
+        console::log_1(&format!("MOT tracker tracks {} objects", tracks.len()).into());
+        console::log_1(&format!("first track box: {}", tracks[0]._box).into());
     }
 }

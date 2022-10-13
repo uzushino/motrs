647809d4046f7745451d0d492e0387a6af1e532e
uzushino <torattoria.mamiya@gmail.com>
2022-10-13 18:08:10 +0900

    update index.js

--- motrs_wasm/index.js
+++ motrs_wasm/index.js
@@ -4,15 +4,14 @@ rust.then((m) => {
     let mot = m.MOT.new();
     let box = [1.0, 1.0, 1.0, 1.0];
 
-    for (let i = 0; i < 2; i ++) {
-        box = box.map(v => v + 1, box)
-        let det = {
-            _box: box,
-        }
-
-        mot.step([det])
-        console.log(i)
-        //let tracks = mot.active_tracks()
-        //console.log(tracks)
+    box = box.map(v => v + 1, box)
+    let det = {
+        _box: box,
     }
+
+    mot.step([det])
+    console.log(i)
+
+    let tracks = mot.active_tracks()
+    console.log(tracks)
 })
